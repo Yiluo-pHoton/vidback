2443eba465e33821619fbdbf3819c196b6f430dd
Yuanchen Li <yli@y-l.me>
2017-09-23 15:11:08 -0400

    update

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var crawler = require('../crawler');
+var nlp = require('../nlp.js');
 
 /*
   post api, the search
@@ -8,6 +9,14 @@ var crawler = require('../crawler');
 */
 router.post('/search', function(req, res, next) {
   crawler.searchHotWord(req.body.q, function(spots){
+    spots.forEach(function(spot){
+      spot.statuses.forEach(status){
+        nlp.getSentiment(status.text, function(){
+          // save the sentiment
+          // return to the client when eveyrhing's ready
+        });
+      }
+    });
     res.json(spots);
   });
 });
